512ad7f78ab5ded9d17d59e9262250238e5c78c9
MonikaPawlik17 <monikapawlik2234@gmail.com>
2025-04-08 18:47:08 +0200

    wip: data structure, get user info function

--- Main.py
+++ Main.py
@@ -1,24 +1,14 @@
-
-
 users=[
-
-
     {"name":"Monika","location":"Mała Wieś","posts":200},
     {"name":"Michał","location":"Kraków","posts":200},
     {"name":"Ksawier","location":"Warszawa","posts":200},
     {"name":"Damian","location":"Poznań","posts":200},
 
-
-
-
-
-
 ]
 
-#twój znajomy Julia z opublikował tyle i tyle postów
-for user in users:
-    print(f"Twój znajomy {user['name']}, z miejscowości {user['location']}, opublikował {user['posts']}")
+def get_user_info(users_data: list)->None:
+    for user in users_data:
+        print(f"Twój znajomy {user['name']}, z miejscowości {user['location']}, opublikował {user['posts']} postów")
 
 
-
-
+get_user_info(users)
