bea6402cfd4381dbe3de19d21335d8b6a83dcbd5
MonikaPawlik17 <monikapawlik2234@gmail.com>
2025-04-08 19:00:58 +0200

    chore: mvt pattern

--- Main.py
+++ Main.py
@@ -1,14 +1,8 @@
-users=[
-    {"name":"Monika","location":"Mała Wieś","posts":200},
-    {"name":"Michał","location":"Kraków","posts":200},
-    {"name":"Ksawier","location":"Warszawa","posts":200},
-    {"name":"Damian","location":"Poznań","posts":200},
+from utils.Controller import get_user_info
+from utils.Model import users
 
-]
+def main():
+    get_user_info(users)
 
-def get_user_info(users_data: list)->None:
-    for user in users_data:
-        print(f"Twój znajomy {user['name']}, z miejscowości {user['location']}, opublikował {user['posts']} postów")
-
-
-get_user_info(users)
+if __name__ == "__main__":
+    main()
