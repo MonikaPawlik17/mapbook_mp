cc16acebffeebebbb145998b56e28c97372c3e39
MonikaPawlik17 <monikapawlik2234@gmail.com>
2025-05-12 09:30:42 +0200

    wip: gui

--- utils/Model.py
+++
@@ -1,7 +0,0 @@
-users=[
-    {"name":"Monika","location":"Lublin","posts":200},
-    {"name":"Michał","location":"Kraków","posts":200},
-    {"name":"Ksawier","location":"Warszawa","posts":200},
-    {"name":"Damian","location":"Poznań","posts":200},
-
-]
