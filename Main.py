5090016813b6ca38f6b6f3e707ed8273b2884480
MonikaPawlik17 <monikapawlik2234@gmail.com>
2025-05-14 17:00:24 +0200

    wip: gui+mapa

--- Main.py
+++ Main.py
@@ -57,7 +57,22 @@ def update_user(i):
     users[i]['posts'] = posts
     show_users()
     button_dodaj_obiekt.config(text='Zapisz', command=add_user(i))
+    entry_imie.delete(0, END)
+    entry_nazwisko.delete(0, END)
+    entry_miejscowosc.delete(0, END)
+    entry_posty.delete(0, END)
     entry_imie.focus()
+
+def show_user_details():
+    i = listbox_lista_obiektow.index(ACTIVE)
+    label_imie_szczegoly_obiektu_wartosc.config(text=users[i]['name'])
+    label_nazwisko_szczegoly_obiektu_wartosc.config(text=users[i]['surname'])
+    label_miejscowosc_szczegoly_obiektu_wartosc.config(text=users[i]['location'])
+    label_posty_szczegoly_obiektu_wartosc.config(text=users[i]['posts'])
+
+
+
+
 
 
 root = Tk()
@@ -82,7 +97,7 @@ label_lista_obiektow.grid(row=0, column=0, )
 listbox_lista_obiektow = Listbox(ramka_lista_obiektow)
 listbox_lista_obiektow.grid(row=1, column=0, columnspan=3)
 
-button_pokaz_szczegoly=Button(ramka_lista_obiektow,text='Pokaż Szczegóły')
+button_pokaz_szczegoly=Button(ramka_lista_obiektow,text='Pokaż Szczegóły', command=show_user_details)
 button_pokaz_szczegoly.grid(row=3, column=0)
 
 button_usun_obiekt=Button(ramka_lista_obiektow,text='Usuń Obiekt',command=remove_user)
