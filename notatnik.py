5349b3a8619c4b2637869cc8d495d55b7847f280
MonikaPawlik17 <monikapawlik2234@gmail.com>
2025-05-14 18:13:28 +0200

    wip: gui+mapa

--- notatnik.py
+++ notatnik.py
@@ -1,14 +1,31 @@
-users=[
-    {"name":"Monika","location":"Mała Wieś","posts":200},
-    {"name":"Michał","location":"Kraków","posts":200},
-    {"name":"Ksawier","location":"Warszawa","posts":200},
-    {"name":"Damian","location":"Poznań","posts":200},
-    ]
+class User:
+    def __init__(self, name:str, surname:str, location:str,posts:str):
+        self.name=name
+        self.surname=surname
+        self.location=location
+        self.posts=posts
+        self.coordinates=self.get_coordinates()
+
+    def get_coordinates(self) -> list:
+        import requests
+        from bs4 import BeautifulSoup
+
+        url = f'https://pl.wikipedia.org/wiki/{self.location}'
+        response = requests.get(url).text
+        response_html = BeautifulSoup(response, 'html.parser')
+        longitude = float(response_html.select('.longitude')[1].text.replace(',', '.'))
+        latitude = float(response_html.select('.latitude')[1].text.replace(',', '.'))
+        return [latitude, longitude]
 
 
 
-# https://pl.wikipedia.org/wiki/Mała_Wieś_(powiat_nowosądecki)
 
+
+
+user_1=User(name='aaa',surname='bbb',location='Warszawa',posts='eee')
+print(user_1.name,user_1.surname,user_1.location,user_1.posts,user_1.coordinates)
+user_2=User(name='ccc',surname='eeb',location='Kraków',posts='ere',)
+print(user_2.name,user_2.surname,user_2.location,user_2.posts,user_2.coordinates)
 
 
 
